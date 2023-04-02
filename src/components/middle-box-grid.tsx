db8e53430875c579cd4421044a0bf4f4220674b6
jbertan <bertanakarsu@gmail.com>
2023-04-02 14:46:13 +0300

    test flex middle and grid

---
+++ src/components/middle-box-grid.tsx
@@ -0,0 +1,4 @@
+const MiddleBoxGrid = () => {
+  return <div className="middle__box__grid">I am at the middle</div>;
+};
+export default MiddleBoxGrid;
--- src/components/middleBox.tsx
+++ src/components/middleBox.tsx
@@ -1,4 +1,4 @@
 const MiddleBox = () => {
-  return <div className="middlebox">I am At middle am i?</div>;
+  return <div className="middle__box__flex">I am At middle am i?</div>;
 };
 export default MiddleBox;
